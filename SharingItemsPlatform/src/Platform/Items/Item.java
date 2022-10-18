46f8459f298f6f0d8f82c27ccfd73021b0d85be1
Antonin Boyon <antonin.boyon@gmail.com>
2022-10-18 11:47:43 +0200

    ItemRessource
    Rest function for Item

--- SharingItemsPlatform/src/Platform/Items/Item.java
+++ SharingItemsPlatform/src/Platform/Items/Item.java
@@ -2,13 +2,15 @@ package Platform.Items;
 import java.util.Date;
 
 public class Item {
+	private String id;
     private Category itemCategory;
     private String itemName;
     private String author;
     private Date releaseDate;
     private int recommendations;
 
-    public Item(Category cat, String itemName, String author, Date releaseDate){
+    public Item(String id, Category cat, String itemName, String author, Date releaseDate){
+    	this.id = id;
         itemCategory = cat;
         this.itemName = itemName;
         this.author = author;
@@ -17,11 +19,15 @@ public class Item {
     }
 
     public String toString(){
-        return "Item name : " + itemName + "\nItem category : " + itemCategory + "\nAuthor : " + author +
+        return "Item name : " + itemName + "\nId : " + id + "\nItem category : " + itemCategory + "\nAuthor : " + author +
                 "\nRelease Date : " + releaseDate + "Users recommendations : " + recommendations;
     }
 
     public Category getItemCategory(){return itemCategory;}
 
     public String getItemName(){return itemName;}
+    
+    public String getId() {
+    	return id;
+    }
 }
--- SharingItemsPlatform/src/dao/ItemDao.java
+++ SharingItemsPlatform/src/dao/ItemDao.java
@@ -7,20 +7,20 @@ import Platform.Items.Category;
 import Platform.Items.Item;
 import java.util.Map;
 
-public class ItemDao {
-	//instance;
+public enum ItemDao {
+	instance;
 	
 	private Map<String, Item> itemProvider = new HashMap<>();
 	
 	@SuppressWarnings("deprecation")
 	private ItemDao () {
-		Item item = new Item(Category.book, "Le grand mort", "Regis Loisel", new Date (2007, 11, 14));
+		Item item = new Item("1", Category.book, "Le grand mort", "Regis Loisel", new Date (2007, 11, 14));
 		itemProvider.put("1", item);
-		item = new Item(Category.DVD, "Shutter Island", "Martin Scorses", new Date (2010, 2, 19));
+		item = new Item("2", Category.DVD, "Shutter Island", "Martin Scorses", new Date (2010, 2, 19));
 		itemProvider.put("2", item);
-		item = new Item(Category.musicCD, "Discovery", "Daft Punk", new Date (2001, 3, 12));
+		item = new Item("3", Category.musicCD, "Discovery", "Daft Punk", new Date (2001, 3, 12));
 		itemProvider.put("3", item);
-		item = new Item(Category.videoGame, "Subnautica", "Unknown Worlds", new Date (2018, 1, 23));
+		item = new Item("4", Category.videoGame, "Subnautica", "Unknown Worlds", new Date (2018, 1, 23));
 		itemProvider.put("4", item);
 	}
 	
