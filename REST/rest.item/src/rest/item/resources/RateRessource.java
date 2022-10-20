4d19e29278a5cfdf05136b3cb619a6ccc2478743
Antonin Boyon <antonin.boyon@gmail.com>
2022-10-20 11:26:04 +0200

    skeleton
    without project and eclipse files

---
+++ REST/rest.item/src/rest/item/resources/RateRessource.java
@@ -0,0 +1,79 @@
+package rest.item.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.JAXBElement;
+
+import rest.item.model.Item;
+import rest.item.dao.ItemDao;
+
+@Path("/rate")
+public class RateRessource {
+	@Context
+	UriInfo uriInfo;
+	@Context
+	Request request;
+	String id;
+	public RateRessource (UriInfo uriInfo, Request request, String id) {
+		this.uriInfo = uriInfo;
+		this.request = request;
+		this.id = id;
+	}
+	
+	// integration of the app
+	@GET
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Item getItem () {
+		Item item = ItemDao.instance.getModel().get(id);
+		if(item==null) {
+			throw new RuntimeException("Get: Item with "+id+" id not found");
+		}
+		return item;
+	}
+	
+	// get function for the browser
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public Item getItemHTML() {
+		Item item = ItemDao.instance.getModel().get(id);
+		if(item==null) {
+			throw new RuntimeException("Get: Item with "+id+" id not found");
+		}
+		return item;
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response putItem(JAXBElement<Item> item) {
+		Item i = item.getValue();
+		return putAndGetResponse(i);
+	}
+	
+	@DELETE
+	public void deleteItem () {
+		Item i = ItemDao.instance.getModel().remove(id);
+		if (i==null) {
+			throw new RuntimeException("Delete: Item with "+id+" id not found");
+		}
+	}
+	
+	private Response putAndGetResponse(Item item) {
+		Response response;
+		if(ItemDao.instance.getModel().containsKey(item.getId())) {
+			response = Response.noContent().build();
+		} else {
+			response = Response.created(uriInfo.getAbsolutePath()).build();
+		}
+		ItemDao.instance.getModel().put(item.getId(), item);
+		return response;
+	}
+}
